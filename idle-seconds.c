fb1fd777ab8e31ac0bd39f3eb5ae418a786e29b4
Panji Kusuma <epanji@gmail.com>
2020-08-14 21:40:18 +0700

    Remove unnecessary code

--- idle-seconds.c
+++ idle-seconds.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
-#include <X11/Xlib.h>
 #include <X11/extensions/scrnsaver.h>
 
 /* gcc idle-seconds.c -o idle-seconds -lX11 -lXext -lXss */
 
-int main(int argc, char *argv[])
+int main()
 {
-  XScreenSaverInfo info;
-  int event_base, error_base;
-
-  Display *display = XOpenDisplay("");
-
-  if (XScreenSaverQueryExtension(display, &event_base, &error_base)) {
+  Display *display = XOpenDisplay(NULL);
+  if (display) {
+    XScreenSaverInfo info;
     XScreenSaverQueryInfo(display, DefaultRootWindow(display), &info);
     printf("%d\n", (int) info.idle/1000);
     return(0);
   } else {
-    fprintf(stderr, "Error: XScreenSaver Extension not present\n");
+    fprintf(stderr, "Display not found\n");
     return(1);
   }
 }
