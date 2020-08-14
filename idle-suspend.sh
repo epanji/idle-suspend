c39d59ad3967193045af377be938e70ed37b1555
Panji Kusuma <epanji@gmail.com>
2020-08-14 22:51:43 +0700

    Add action when display not found

--- idle-suspend.sh
+++ idle-suspend.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Author: Panji Kusuma <epanji at gmail dot com>
 #
@@ -15,8 +15,33 @@ PREFIX=/usr
 BINARY_NAME=idle-seconds
 BINARY=$PREFIX/bin/$BINARY_NAME
 
+AWK_ACTION='{
+if (NR!=1) {
+  if ($4 ~ /days/) {
+    split($4,d,"days");
+    print d[1]*86400;
+  } else if ( $4 ~ /:|s/) {
+    if ($4 ~/s/) {
+      sub(/s/,"",$4);
+      split($4,s,".");
+      print s[1];
+    } else if ( $4 ~/m/) {
+      split($4,m,":");
+      print (m[1]*60+m[2])*60
+    } else {
+      split($4,m,":");
+      print m[1]*60+m[2];
+    }
+  } else {
+    print $4;
+  }
+}}'
+
 if [ -x $BINARY ]; then
     IDLE_SECONDS=$($BINARY)
+    if [ $? -ne 0 ]; then
+        IDLE_SECONDS=$(w -sh | awk "$AWK_ACTION" | sort -n | head -n 1)
+    fi
     if [ $IDLE_SECONDS -ge $SECONDS ]; then
         dbus-send --system --print-reply --dest='org.freedesktop.UPower' /org/freedesktop/UPower org.freedesktop.UPower.Suspend
     else
